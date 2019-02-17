f1fa1c4303e7a26657af269b691ac8d055a2b07d
Tal Barak <tal63566@gmail.com>
2019-02-17 14:00:13 +0200

    gallery structure

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,3 +1,25 @@
 'use strict'
 
 
+function init() {
+    renderMemes()
+}
+
+function renderMemes() {
+    var strHtml = ''
+    var hex= document.querySelector('.hexagon-container')
+    for (var i = 0; i < gMemes.length; i++) {
+        strHtml += `
+            <div class="hexagon" onclick="onPickMeme(event, '${gMemes[i].id}')">
+                <div class="hexagon-inside">
+                    <div class="hexagon-img" style="background-image: url(${gMemes[i].url})" ></div>
+                </div>
+            </div>`
+        }
+        hex.innerHTML=strHtml
+}
+
+function onPickMeme(val, id){
+    console.log(val, id)
+    
+}
--- js/gallery-service.js
+++ js/gallery-service.js
@@ -1,1 +1,48 @@
 'use strict'
+
+var gMemes = [
+    {
+        id: makeId(),
+        url: 'img/patrick.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/2.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/3.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/4.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/5.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/img11.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/X-Everywhere.jpg'
+    },
+    {
+        id: makeId(),
+        url: 'img/drevil.jpg'
+    }
+]
+
+function makeId() {
+    var length = 6;
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return txt;
+}
